feae996bd18b3893f6e70e6e16a4a7da42513134
Genie Yang <genieyang93@gmail.com>
2018-11-09 23:45:20 -0500

    Create basic html structure and css

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+//Create stocksList array that list stock symnbols that interests me
+const stocksList = [
+    {
+        symbol: 'GE',
+        name: 'General Electric'
+    },
+    {
+        symbol: 'AAPL',
+        name: 'Apple'
+    },
+    {
+        symbol: 'MSFT',
+        name: 'Microsoft'
+    },
+    {
+        symbol: 'SBUX',
+        name: 'Starbucks'
+    },
+    {
+        symbol: 'AMZN',
+        name: 'Amazon'
+    },
+    {
+        symbol: 'GOOGL',
+        name: 'Google'
+    },
+    {
+        symbol: 'FB',
+        name: 'Facebook'
+    },
+    {
+        symbol: 'IBM',
+        name: 'IBM'
+    },
+   
+]
